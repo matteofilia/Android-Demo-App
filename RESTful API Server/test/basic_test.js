4ce21c714e03da48b3472ecc768f704768482c4d
matteofilia <matteo@matteofilia.com>
2019-03-13 13:16:17 -0400

    Setup web server testing with mocha

---
+++ RESTful API Server/test/basic_test.js
@@ -0,0 +1,12 @@
+const chai = require("chai")
+const assert = chai.assert
+const expect = chai.expect
+
+describe("Really Really Simple Test", function() {
+  describe("Assert", function() {
+   it("zero equals zero", function() {
+     // Don't throw any exceptions
+     assert.equal(0,0)
+   })
+  })
+})
